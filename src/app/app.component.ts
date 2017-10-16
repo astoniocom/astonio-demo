ab909ebcdaeea4b365cf66e3d61884d844381e2c
Viktar Katlianik <7532425@gmail.com>
2017-10-16 17:52:08 +0300

    Astonio QuickStart guide completed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewContainerRef, ChangeDetectorRef, OnInit, ViewChild } from '@angular/core';
+import { WindowsManager } from '@astonio/ui';
+import { Backend, ListModel } from '@astonio/core';
+import { ModelWindowsDispatcher } from '@astonio/model-ui';
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  @ViewChild('container', { read: ViewContainerRef }) containerVCR;
+  tables:ListModel[] = [];
+ 
+  constructor(private wm:WindowsManager, private mwd:ModelWindowsDispatcher, private vcr:ViewContainerRef, private backend:Backend, private cdr:ChangeDetectorRef) {
+    this.backend.bootstrapped.subscribe(() => {
+      this.tables = [...this.backend.listModels.values()]; 
+      this.cdr.detectChanges();
+    });	
+  }
+ 
+  openListWindow(listModel:ListModel) {
+    var wndClass = this.mwd.getListWindow(listModel);
+    var wnd = this.mwd.getListWindow(listModel, false, null, true).subscribe(wndInfo => {
+      new wndInfo.window(this.wm, null, Object.assign({}, wndInfo.windowOptions, {list:listModel}));
+    });
+  }
+ 
+  ngOnInit() {
+    this.wm.initVCR(this.containerVCR); 
+  }
 }
